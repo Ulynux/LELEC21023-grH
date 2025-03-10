2d5d4c2450bfcd780dffdfaff810dc2e3638696d
Ulysse <ulysse.allard@student.uclouvain.be>
2025-03-10 14:57:25 +0100

    Actu

--- mcu/hands_on_audio_acquisition/Core/Src/main.c
+++ mcu/hands_on_audio_acquisition/Core/Src/main.c
@@ -38,7 +38,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define ADC_BUF_SIZE 24000
+#define ADC_BUF_SIZE 25000
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
