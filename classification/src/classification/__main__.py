0c4f61c5a307c0f1d5cfcd29d7a6d21f53510f2a
Ulysse <ulysse.allard@student.uclouvain.be>
2025-03-31 09:31:43 +0200

    essai rye run classify

--- classification/src/classification/__main__.py
+++ classification/src/classification/__main__.py
@@ -56,59 +56,58 @@ def main(
     """
     with open('classification/data/models/best_rf_model.pickle', 'rb') as file:
         model_rf = pickle.load(file)
-    with open('classification/data/models/pca_25_components.pickle', 'rb') as file:
-        model_pca = pickle.load(file)
+        with open('classification/data/models/pca_25_components.pickle', 'rb') as file:
+            model_pca = pickle.load(file)
 
    
 
-    for payload in _input:
-        if PRINT_PREFIX in payload:
-            payload = payload[len(PRINT_PREFIX) :]
+            for payload in _input:
+                if PRINT_PREFIX in payload:
+                    payload = payload[len(PRINT_PREFIX) :]
 
-            melvec = payload_to_melvecs(payload, melvec_length, n_melvecs)
-            logger.info(f"Parsed payload into Mel vectors: {melvec}")
-            memory = []
+                    melvec = payload_to_melvecs(payload, melvec_length, n_melvecs)
+                    logger.info(f"Parsed payload into Mel vectors: {melvec}")
+                    memory = []
 
-            if model_rf :
-                melvec = melvec.copy()
-                melvec = melvec.astype(np.float64)
-                print(melvec)
+                    melvec = melvec.copy()
+                    melvec = melvec.astype(np.float64)
+                    print(melvec)
 
-                # Normalize the melvec
-                melvec -= np.mean(melvec)
-                melvec = melvec / np.linalg.norm(melvec)
+                    # Normalize the melvec
+                    melvec -= np.mean(melvec)
+                    melvec = melvec / np.linalg.norm(melvec)
 
-                # Reshape melvec to 2D before PCA transformation
-                melvec = melvec.reshape(1, -1)
-                print(melvec.shape)
-                # Apply PCA transformation
-                melvec = model_pca.transform(melvec)
+                    # Reshape melvec to 2D before PCA transformation
+                    melvec = melvec.reshape(1, -1)
+                    print(melvec.shape)
+                    # Apply PCA transformation
+                    melvec = model_pca.transform(melvec)
 
-                # Predict probabilities and class
-                proba_rf = model_rf.predict_proba(melvec)
-                prediction = model_rf.predict(melvec)
-                memory.append(proba_rf)
-                if len(memory) > 5:
-                    memory.pop(0)
+                    # Predict probabilities and class
+                    proba_rf = model_rf.predict_proba(melvec)
+                    prediction = model_rf.predict(melvec)
+                    memory.append(proba_rf)
+                    if len(memory) > 5:
+                        memory.pop(0)
 
-                memory_array = np.array(memory)
+                    memory_array = np.array(memory)
 
-                memory.append(proba_rf)
+                    memory.append(proba_rf)
 
-                memory_array = np.array(memory)
+                    memory_array = np.array(memory)
 
-                majority_class_index = np.bincount(np.argmax(memory_array, axis=2).flatten()).argmax()
-                majority_class = model_rf.classes_[majority_class_index]
-                if majority_class == "gun" :
-                    majority_class = "gunshot"
-                if memory_array.size > 1:
-                    logger.info(f"Predictions: {majority_class}")
-                    
-                    answer = requests.post(f"{hostname}/lelec210x/leaderboard/submit/{key}/{majority_class}", timeout=1)
-                    
-                    json_answer = json.loads(answer.text)
-                    print(json_answer)
-                    memory.clear()
-                    wait_iterations = np.random.randint(1, 3)
-                    for _ in range(wait_iterations):
-                        next(_input)
+                    majority_class_index = np.bincount(np.argmax(memory_array, axis=2).flatten()).argmax()
+                    majority_class = model_rf.classes_[majority_class_index]
+                    if majority_class == "gun" :
+                        majority_class = "gunshot"
+                    if memory_array.size > 1:
+                        logger.info(f"Predictions: {majority_class}")
+                        
+                        answer = requests.post(f"{hostname}/lelec210x/leaderboard/submit/{key}/{majority_class}", timeout=1)
+                        
+                        json_answer = json.loads(answer.text)
+                        print(json_answer)
+                        memory.clear()
+                        wait_iterations = np.random.randint(1, 3)
+                        for _ in range(wait_iterations):
+                            next(_input)
--- mcu/hands_on_feature_vectors/soundlaucher.py
+++ mcu/hands_on_feature_vectors/soundlaucher.py
@@ -8,27 +8,4 @@ def play_sound(file_path):
     sd.play(data, fs)
     sd.wait()  # Wait until the sound has finished playing
 
-if __name__ == "__main__":
-    count = 0
-    # Iterate over the dataset and count the number of matching files
-    for root, dirs, files in os.walk("data/"):
-        for file in files:
-            if file.endswith(".wav") and "chainsaw" in file:
-                count += 1
-    print(f"Number of .wav files containing 'chainsaw': {count}")
-    argParser = argparse.ArgumentParser()
-    argParser.add_argument("-d", "--dataset", default="data/", help="Path to the dataset directory")
-    args = argParser.parse_args()
-    print("play_sounds launched...\n")
-    count = 0
-    # Iterate over the dataset and play each sound
-    for root, dirs, files in os.walk(args.dataset):
-    
-        for file in files:
-            if file.endswith(".wav") and "gun" in file :#and not "orks" in file:  # Ensure only .wav files are processed
-                print(count)
-                count += 1
-                print(f"Processing file: {file}")
-                file_path = os.path.join(root, file)
-                print(f"Playing sound: {file_path}")
-                play_sound(file_path)
+play_sound("classification/src/classification/datasets/soundfiles/background.wav")
