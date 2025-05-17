1c07b48acfe352fbb3a32e2b2fab13cc1c09b012
Matthieu <matthieu.absil@student.uclouvain.be>
2025-05-17 18:58:22 +0200

    bon per.py

--- telecom/hands_on_simulation/PER.py
+++ telecom/hands_on_simulation/PER.py
@@ -3,53 +3,65 @@ import matplotlib.pyplot as plt
 
 
 def extract_data(file_path):
-    # Lire le fichier TXT et extraire les colonnes spécifiques
     try:
         with open(file_path, 'r') as file:
             data = file.readlines()
             extracted_data = []
-            for line in data:
+            ignored_lines = 0
+            
+            for line in data[1:]:
                 parts = line.strip().split()
-                if len(parts) >= 6 and parts[0].replace('.', '', 1).isdigit():
-                    cfo = float(parts[0])
-                    snr = float(parts[2])
-                    txp = int(parts[3])
-                    ber = float(parts[4])
-                    invalid = int(parts[5])
-                    extracted_data.append((cfo, snr, txp, ber, invalid))
+                cfo = float(parts[0])
+                snr = float(parts[2])
+                txp = int(parts[3])
+                ber = float(parts[4])
+                invalid = int(parts[5])
+                extracted_data.append((cfo, snr, txp, ber, invalid))
+
+            
+
+            
         return extracted_data
     except Exception as e:
         print(f"Erreur lors de la lecture du fichier : {e}")
 
 
-file_path = 'Data.txt'
+
+file_path = 'telecom/Data.txt'
+
 # Remplace par le chemin réel de ton fichier
 data_exp = extract_data(file_path)
-
+print(len(data_exp))
 cfo = []
 snr = []
 txp = []
 ber = []
 invalid = []
 
-cfo, snr, txp, ber, invalid = zip(*data_exp)
-txp_count = dict(Counter(txp))
 
+cfo, snr, txp, ber, invalid = zip(*data_exp)
+
+
+txp_count = dict(Counter(txp))
 
 key = list(txp_count.keys())
 occurence = []
 for i in range(len(key)):
     occurence.append(txp_count.get(key[i]))
 
+
+
 SNR = []
 INVALID = []
 CFO = []
 index = 0
 
+
+
 snr = list(snr)
 invalid = list(invalid)
 cfo = list(cfo)
-print(occurence)
+
 for i in range(0,len(occurence)):
     SNR.append(snr[:occurence[i]])
     del snr[:occurence[i]]
@@ -60,8 +72,8 @@ for i in range(0,len(occurence)):
 
     packet = 0
 for i in range(len(INVALID)):
-    if(len(INVALID[i]) != 100):
-        for j in range(100 - len(INVALID[i])):
+    if(len(INVALID[i]) != 1000):
+        for j in range(1000 - len(INVALID[i])):
             INVALID[i].append(1)
 
 SNR_aver = []
@@ -76,23 +88,34 @@ for i in range(len(INVALID)):
     packet = 0
     for j in range(len(INVALID[i])):
         packet+=INVALID[i][j]
-    PACKET_ERROR.append(packet/100)
+    PACKET_ERROR.append(packet/1000)
 
 
+
+
+print("SNR : ",SNR_aver)
+print("PER : ",PACKET_ERROR)
 plt.figure()
-plt.hist(cfo,bins = 20)
-plt.xlabel("CFO")
-plt.ylabel("OCCURENCe")
-plt.savefig('telecom/CFO.png')
+plt.plot(SNR_aver,PACKET_ERROR,marker='o',linestyle = '-')
 
 
-plt.figure()
-plt.plot(SNR_aver,PACKET_ERROR,'o',linestyle = '-')
-plt.yscale('log')
+
 plt.grid(True)
 plt.xlabel('SNR')
 plt.ylabel('PER')
-plt.savefig('telecom/test_packet.png')
+#plt.savefig('telecom/test_packet.png')
+
+plt.show()
+plt.figure()
+plt.plot(SNR_aver,PACKET_ERROR,marker='o',linestyle = '-')
+
+
+
+plt.grid(True)
+plt.xlabel('SNR')
+plt.ylabel('PER')
+plt.yscale('log')
+#plt.savefig('telecom/test_packet.png')
 
 plt.show()
 
