1ff15735d9e0ec231c22bb72c5bc3b2a0162621d
lraviart <louis.raviart@student.uclouvain.be>
2025-05-18 09:00:30 +0200

    new measurements

--- mcu/hands_on_main_app/Core/Inc/eval_radio.h
+++ mcu/hands_on_main_app/Core/Inc/eval_radio.h
@@ -6,7 +6,7 @@
 #define INC_EVAL_RADIO_H_
 
 // Radio evaluation parameters
-#define MIN_PA_LEVEL -30 // initial Tx transmit power, in dBm
+#define MIN_PA_LEVEL -29 // initial Tx transmit power, in dBm
 #define MAX_PA_LEVEL -20 // final Tx transmit power, in dBm
 #define N_PACKETS 1000 // number of packets transmitted for each Tx power level
 #define PAYLOAD_LEN 100 // payload length of the transmitted packets
